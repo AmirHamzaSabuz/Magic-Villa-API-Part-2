3113000435a19f1ca3419ef7d403694a391b968b
Amir Hamza <hamza_amir@yahoo.com>
2023-07-05 23:54:59 +0600

    Section 2 - First API Endpoint - HTTPGET

--- MagicVilla_API/Controllers/VillaAPIController.cs
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_API.Controllers
 {
     [Route("api/[controller]")]
+    //[Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1,Name="Pool View" },
+                new Villa{Id=2,Name="Beach View" }
+            };
+        }
     }
 }
